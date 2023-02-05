33876dc13a4f32cb5f7ffc0ed86305c20c25dc51
Zeming Xie <xiezem000life@gmail.com>
2023-02-05 11:47:32 -0600

    update methods and comments

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,6 +1,9 @@
+from ast import List
 import pygame
 import numpy as np
 import random
+
+SQAURE_PIXEL_SIZE = 30
  
 def main():
 
@@ -9,8 +12,9 @@ def main():
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("Mine Sweeper")
 
-    screen = initialize_board(10, 10)
-     
+    width, height = (10, 10)
+    screen = initialize_board(width, height)
+
     # define a variable to control the main loop
     running = True
      
@@ -32,16 +36,30 @@ def main():
 def initialize_board(width: int, height: int) -> pygame.Surface: 
     darkGreen = pygame.Color(38, 206, 2)
     lightGreen = pygame.Color(119, 255, 90)
-    RectSize = 30
-    screenWidth = RectSize * width
-    screenHeight = RectSize * height
+    screenWidth = SQAURE_PIXEL_SIZE * width
+    screenHeight = SQAURE_PIXEL_SIZE * height
     screen = pygame.display.set_mode((screenWidth,screenHeight))
     screen.fill(lightGreen)
     for row in range(width):
         for col in range(row % 2, width, 2):
-            pygame.draw.rect(screen, darkGreen, (col * RectSize, row*RectSize, RectSize, RectSize))
+            darkRect = pygame.Rect(col * SQAURE_PIXEL_SIZE, row * SQAURE_PIXEL_SIZE, SQAURE_PIXEL_SIZE, SQAURE_PIXEL_SIZE)
+            pygame.draw.rect(screen, darkGreen, darkRect)
     pygame.display.flip()
-    return screen
+
+####
+# Initialize numbers to a picture asset. Use https://stackoverflow.com/questions/20842801/how-to-display-text-in-pygame
+# as a guide. This function will return a list of text Surface 1-8 for later use
+####
+def initialize_numbers() -> list[pygame.Surface]:
+    return list()
+
+####
+# Initialize revealed board. revealed board should be an int ndarray with size (width, height),
+# -3 -> mine, -2 -> flag, -1 -> unrevealed, 0 - 8 -> revealed with number of surrounding mines as the number
+# revealed board should start with all -1s.
+####
+def initialize_reveal_board(width: int, height: int) -> np.ndarray:
+    return null
 
 ####
 # Generate the mines. width and height is the size of the board, x and y is the 
@@ -63,6 +81,27 @@ def generate_mine(width: int, height: int, x: int, y: int, mineNum: int) -> np.n
     
     return board
 
+####
+# Highlight the sqaure where the curser is at. If the curser is at a revealed location, do nothing.
+# Make sure to redraw the screen first to get rid of previous highlighted portion. You can use 
+# pygame.mouse.get_pos() to get the mouse position.
+####
+def highlight_square(screen: pygame.Surface, revealed_board: np.ndarray):
+    return null
+
+####
+# x and y is the user clicked location, update the revealed board, return true when
+# no mine is hit, return false when user clicked on mine
+####
+def reveal_board(mines: np.ndarray, revealed_board: np.ndarray, x: int, y: int) -> bool:
+    return null
+
+####
+# draw revealed board, for now mine use red color, flag use yellow color, and 0-8 will
+# use light brown and dark brown as background color. For 1-8, also draw the number on top
+####
+def draw_board(screen: pygame.Surface, revealed_board: np.ndarray, number_image: list[pygame.Surface]):
+    return null
      
 if __name__=="__main__":
     main()
