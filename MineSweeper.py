ae9fdf8b819298a9c0d357dae6be6153d7097009
Yunyan Yao <helenyao111@gmail.com>
2023-02-05 09:51:43 -0600

    Added initialize_board

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import random
  
 def main():
 
@@ -7,12 +8,8 @@ def main():
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("Mine Sweeper")
-     
-    screen = pygame.display.set_mode((300,300))
 
-    draw_board(screen, 10, 10)
-
-    pygame.display.flip()
+    screen = initialize_board(10, 10)
      
     # define a variable to control the main loop
     running = True
@@ -26,17 +23,45 @@ def main():
                 # change the value to False, to exit the main loop
                 running = False
 
-def draw_board(screen: pygame.Surface, width: int, height: int):
+####
+# Initialize screen based on size passed in, and draw the board,
+# return the screen. Width and height represent how many squares
+# there should be. I.e. width of 10 and height of 10 is a 10x10
+# board.
+####
+def initialize_board(width: int, height: int) -> pygame.Surface: 
     darkGreen = pygame.Color(38, 206, 2)
     lightGreen = pygame.Color(119, 255, 90)
-    startRect = pygame.Rect(0, 0, 30, 30)
-    pygame.draw.rect(screen, darkGreen, startRect)
-    pygame.draw.rect(screen, lightGreen, pygame.Rect(30, 0, 30, 30))
+    RectSize = 30
+    screenWidth = RectSize * width
+    screenHeight = RectSize * height
+    screen = pygame.display.set_mode((screenWidth,screenHeight))
+    screen.fill(lightGreen)
+    for row in range(width):
+        for col in range(row % 2, width, 2):
+            pygame.draw.rect(screen, darkGreen, (col * RectSize, row*RectSize, RectSize, RectSize))
+    pygame.display.flip()
+    return screen
 
+####
+# Generate the mines. width and height is the size of the board, x and y is the 
+# initial position user clicked, make sure no mines are populated 1 space surrending
+# the initial position and the initial position. The output should be a numpy 
+# array of size (width, height), and true represent mine present, false represent mine absent.
+# mineNum represent the number of mines. 
+####
+def generate_mine(width: int, height: int, x: int, y: int, mineNum: int) -> np.ndarray:
+    #board_coordinates = [(i, j) for i in range(0,width) for j in range(0, height)]
+    #mine_coordinates = random.sample(board_coordinates, mineNum)  
+    board = np.array([[0 for i in range(width)] for j in range(height)])
+    board[x, y] = 'b'
+    adj_squares = board[max((y_pos-1),0):min(height + 2, y_pos + 2), max((x_pos-1),0):min(width + 2, x_pos + 2)]
+    for i in range(mineNum):
+        while True:
+            x_pos, y_pos = random.randint(0, width), random.randint(0,height)
+            if board[y_pos, x_pos] != 'b':
+                board[y_pos, x_pos] = 'b'
+                break
      
-     
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
 if __name__=="__main__":
-    # call the main function
     main()
